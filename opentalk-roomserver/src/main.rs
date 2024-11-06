769ce8febaafe33548c26a67674e48819e3ab879
Albrecht Weiche <a.weiche@opentalk.eu>
2024-11-06 16:46:13 +0100

    docs: introduce basic crate level documentation

--- opentalk-roomserver/src/main.rs
+++ opentalk-roomserver/src/main.rs
@@ -2,6 +2,8 @@
 // SPDX-FileCopyrightText: OpenTalk Team <mail@opentalk.eu>
 // SPDX-FileCopyrightText: Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
 
+//! This crate builds an executable that runs the RoomServer. It implements the [_OpenTalk RoomServer Web API_][opentalk_roomserver_web_api].
+
 use anyhow::{Context, Result};
 use clap::Parser;
 use cli::{Args, SubCommand};
