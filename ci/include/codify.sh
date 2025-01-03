5f545ac7d99285efed51fcb78c843a8b203e992b
Albrecht Weiche <a.weiche@opentalk.eu>
2025-01-03 08:41:10 +0100

    ci: verify documentation is in sync with files

---
+++ ci/include/codify.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+# SPDX-License-Identifier: EUPL-1.2
+
+codify() {
+  if [ -z "$1" ]; then
+    echo "Error: no language specified"
+    return 1
+  fi
+
+  echo "\`\`\`$1"
+  # print out data passed to stdin
+  cat -
+  echo '```'
+}
