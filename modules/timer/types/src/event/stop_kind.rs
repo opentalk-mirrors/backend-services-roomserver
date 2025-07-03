d6eba06b5632b054957e08fec5cd16a495e502d0
Sandro Forster <s.forster@opentalk.eu>
2025-07-03 16:58:28 +0300

    feat(client): add timer types

--- modules/timer/types/src/event/stop_kind.rs
+++ modules/timer/types/src/event/stop_kind.rs
@@ -6,7 +6,7 @@ use opentalk_types_signaling::ParticipantId;
 use serde::{Deserialize, Serialize};
 
 /// The stop reason
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", tag = "kind", content = "participant_id")]
 pub enum StopKind {
     /// The timer has been stopped by a moderator
--- modules/timer/types/src/event/updated_ready_status.rs
+++ modules/timer/types/src/event/updated_ready_status.rs
@@ -6,7 +6,7 @@ use opentalk_types_signaling::ParticipantId;
 use serde::{Deserialize, Serialize};
 
 /// Update the ready status
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct UpdatedReadyStatus {
     /// The participant that updated its status
     pub participant_id: ParticipantId,
